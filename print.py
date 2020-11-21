efe28ea3b3885e23f3aebaf15e6c514ed0d114f1
Benjamin G Nechicattu <benjaminnechicattu@gmail.com>
2020-11-21 15:55:10 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,60 @@
+print("Today we see print functions in python")
+print()
+
+################################### type? #######################################
+#print(type(print))
+
+
+#print('tutorial','on','python','print','function')              ## default seperation is space
+#print('tutorial','on','python','print','function',sep='\n')     ## new line char
+#print('tutorial','on','python','print','function',sep=',')
+#print('tutorial','on','python','print','function',sep='+')
+#print(2,1,6,5,4,sep='+')
+#a = 1
+#b = 2
+#print(a,b,sep='+')
+
+###     Default print ends with   new line char, \n   ###
+
+#str1 = 'tutorial on'
+#str2 = 'python print function'
+
+#print(str1)
+#print(str2)
+
+#print(str1,end=' ')
+#print(str2)
+
+############################    printing  values       #############################
+
+#a = 2
+#b = "codershouse"
+
+#print(a,"is an integer while",b,"is a string.")
+#print("{0} is an integer while {1} is a string.".format(a,b))
+#print("{1} is an integer while {1} is a string.".format(a,b))
+#print("%d is an integer while %s is a string."%(a,b))
+
+############################################# fin ################################################
+
+
+
+########################################### extras ###############################################
+
+########## print date ###########
+
+#import datetime
+
+#x = datetime.datetime.now()
+
+""" expected output :
+
+    today's date and time is : 2020-11-21 10:17:44.526598
+"""
+
+"""
+milti line comment in python
+milti line comment in python
+milti line comment in python
+"""
+
